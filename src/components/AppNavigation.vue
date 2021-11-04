de16b6822e79914e84624c2c879a6e170910eca1
早川修平 <hayakawashuuhei@hayakawashuuheinoMacBook-Pro.local>
2021-11-04 19:05:18 +0900

    Add AppNavigation.vue for click on filter botton and mypage botton

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer
+   padless
+   dark
+   fixed
+  >
+
+    <v-row
+     justify="space-around"
+     no-gutters
+    >
+
+     <v-btn
+      v-for="icon in icons"
+      :key="icon"
+      rounded
+      class="my-2"
+     >
+     <v-icon size="24px">
+     {{icon}}
+     </v-icon>
+     </v-btn>
+    </v-row>
+  </v-footer>
+</template>
+
+
+<script>
+export default {
+    name:'AppNavigation',
+
+    data: () => ({
+      icons: [
+        'mdi-filter-variant',
+        'mdi-account',
+      ],
+  }),
+}
+</script>
